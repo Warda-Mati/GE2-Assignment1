4849a51a86377e75600a8ebc03d79dcc89925d93
Warda-Mati <46999666+Warda-Mati@users.noreply.github.com>
2021-02-18 13:35:32 +0000

    started project

---
+++ Assets/Fog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.fogColor = new Color (0.22f, 0.65f, 0.77f, 0.5f);
+        RenderSettings.fogDensity = 0.12f;
+        RenderSettings.fog = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    // 4 points 
+    public List<Vector3> waypoints;
+    
+
+    private void Awake()
+    {
+        // first point 
+        
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            waypoints.Add(this.gameObject.transform.GetChild(i).position); 
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(waypoints[0], waypoints[1]);
+        Gizmos.DrawLine(waypoints[1], waypoints[2]);
+        Gizmos.DrawLine(waypoints[2], waypoints[0]);
+       
+    }
+}
