401b09322a8e272dfcf9c105c8b58cdbcbdcce6f
Warda-Mati <c17323991@mytudublin.ie>
2021-03-31 18:20:32 +0100

    attempted flocking

--- Assets/Scripts/Flocking.cs
+++ Assets/Scripts/Flocking.cs
@@ -10,38 +10,30 @@ public class Flocking : MonoBehaviour
     public Vector3 area;
 
     public GameObject[] allFish;
+
+    public List<GameObject> allfishes;
     // Start is called before the first frame update
     void Start()
     {
         allFish = new GameObject[flockSize];
+        allfishes = new List<GameObject>();
         for (int i = 0; i < flockSize; i++)
         {
             Vector3 pos = Random.insideUnitSphere;
             pos = new Vector3(pos.x * area.x, pos.y * area.y, pos.y * area.y);
             Vector3 fishPos = transform.position + pos;
             allFish[i] = Instantiate(fish, fishPos, transform.rotation);
+            allFish[i].AddComponent<FlockingBehaviour>();
+            allfishes.Add(allFish[i]);
+        }
+
+        for (int i = 0; i < allfishes.Count; i++)
+        {
+            allfishes[i].GetComponent<FlockingBehaviour>().agents = allfishes;
         }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    /*public void Seperation()
-    {
-        for (int i = 0; i < tagged.Count; i++)
-        {
-            entity = tagged[i];
-        }
-
-        if (entity != null)
-        {
-            toEntity = agent.pos - entity.pos;
-            steeringForce += toEntity.normalize / toEntity
-        }
-    }*/
+  
     
 }
---
+++ Assets/Scripts/FlockingBehaviour.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingBehaviour : SteeringBehavior
+{
+    public List<GameObject> agents;
+    
+    public override Vector3 Calculate()
+    {
+        return Seperation() + Cohesion() + Alignment();
+    }
+
+    public Vector3 Seperation()
+    {
+        Vector3 steeringForce = Vector3.zero;
+        for (int i = 0; i < agents.Count; i++)
+        {
+            GameObject entity = agents[i];
+            if (entity != null)
+            {
+                Vector3 toEntity = transform.position - entity.transform.position;
+                steeringForce += Vector3.Normalize(toEntity) / toEntity.magnitude;
+            }
+        }
+
+        checkNaN(steeringForce);
+        return steeringForce;
+    }
+
+    public Vector3 Cohesion()
+    {
+        Vector3 steeringForce = Vector3.zero;
+        Vector3 centreOfMass = Vector3.zero;
+        int neighbourCount = 0;
+        foreach (GameObject entity in agents)
+        {
+            if (entity != gameObject)
+            {
+                centreOfMass += entity.transform.position;
+                neighbourCount++;
+            }   
+        }
+
+        if (neighbourCount > 0)
+        {
+            centreOfMass /= (float)neighbourCount;
+            if (centreOfMass.sqrMagnitude == 0)
+            {
+                steeringForce = Vector3.zero;
+            }
+            else
+            {
+                steeringForce = Vector3.Normalize(boid.SeekForce(centreOfMass));     
+            }
+        }
+
+        checkNaN(steeringForce);
+        return steeringForce;
+    }
+
+    public Vector3 Alignment()
+    {
+        Vector3 steeringForce = Vector3.zero;
+        int neighbourCount = 0;
+
+        foreach (GameObject agent in agents)
+        {
+            if (agent != gameObject)
+            {
+                steeringForce += agent.transform.position;
+                neighbourCount++;   
+            }
+        }
+
+        if (neighbourCount > 0)
+        {
+            steeringForce /= (float) neighbourCount;
+            steeringForce = steeringForce - transform.forward;
+        }
+       
+        return steeringForce;
+    }
+    
+    static public bool checkNaN(Vector3 v)
+    {
+        if (float.IsNaN(v.x))
+        {
+            Debug.Log("Nan x");
+            return true;
+        }
+        if (float.IsNaN(v.y))
+        {
+            Debug.Log("Nan y");
+            return true;
+        }
+        if (float.IsNaN(v.z))
+        {
+            Debug.Log("Nan z");
+            return true;
+        }
+        return false;
+    }
+
+}
