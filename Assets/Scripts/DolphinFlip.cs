90c57c4088b91e2e64a97120613d1d696f2d8195
Warda-Mati <c17323991@mytudublin.ie>
2021-04-12 08:27:21 +0100

    dolphin flip

---
+++ Assets/Scripts/DolphinFlip.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DolphinFlip : MonoBehaviour
+{
+    public float rotation = 90;
+    public bool rotating;
+    public float speed = 0.2f;
+
+    private Quaternion rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = new Quaternion(rotation, 0.0f, 0.0f,0.0f);
+        //StartCoroutine(RotateImage());
+        
+        
+        //Debug.Log(transform.rotation.x + " > " + y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveSpeed = 0.2f;
+        float y = -270;
+        Vector3 target = new Vector3(y,0,0);
+        //Debug.Log(target);
+        transform.eulerAngles = Vector3.Lerp(transform.position,target, moveSpeed * Time.time);
+    }
+    
+ 
+ 
+    /*IEnumerator RotateImage()
+    {
+        float moveSpeed = 0.00005f;
+        float y = -240;
+        Quaternion target = new Quaternion(y,transform.rotation.y,transform.rotation.z,0);
+        Debug.Log(transform.rotation.x + " > " + y);
+        //while(transform.rotation.x > y)
+        //{
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, moveSpeed * Time.time);
+        //yield return null;
+        //}
+        //yield return null;
+    }*/
+}
